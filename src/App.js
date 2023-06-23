c46848db6890235f34a588d05a9944f883a3cb20
orenSampson <oren.sampson@gmail.com>
2023-06-23 19:44:48 +0300

    basic router added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import HomePage from './pages/HomePage/HomePage';
+import UserProfile from './pages/UserProfile/UserProfile';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage />, errorElement: <ErrorPage /> },
+  { path: '/user-profile', element: <UserProfile /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
